dc7ef778b44d4be66b98b7386cb3a4702c9a41df
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-02-20 17:55:03 -0300

    arrow function

---
+++ javascript-avancado/arrow.js
@@ -0,0 +1,17 @@
+let dobro = () => { console.log(this)};
+
+let dobro = x =>  2 * x;
+
+// let dobro = function () {
+//     console.log(this)
+// }
+
+let numero = {
+    x: 8,
+    d: dobro
+}
+
+dobro = dobro.bind(numero)
+
+numero.d()
+// console.log(numero.d());
