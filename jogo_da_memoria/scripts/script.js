acb92fa8b8251e633a5d646c91eb1b725aee7296
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-04-03 17:56:51 -0300

    separando a view da lógica

--- jogo_da_memoria/scripts/script.js
+++ jogo_da_memoria/scripts/script.js
@@ -3,27 +3,12 @@ const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = ['bootstrap',
-    'CSS',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'
-];
-
-let cards = null;
-
 startGame();
 
 function startGame(){
-    cards = createCardsFromTechs(techs);
-    shuffleCards(cards);
-    console.log(cards);
-    initializeCards(cards);
+    // shuffleCards(cards);
+    // console.log(cards);
+    initializeCards(game.createCardsFromTechs());
 }
 
 function initializeCards(cards) {
@@ -31,7 +16,7 @@ function initializeCards(cards) {
     
     // criar as cartas
 
-    cards.forEach(card => {
+    game.cards.forEach(card => {
 
         let cardElement = document.createElement('div');
         cardElement.id = card.id;
@@ -44,7 +29,9 @@ function initializeCards(cards) {
         gameBoard.appendChild(cardElement);
     })
 
-    console.log(gameBoard);
+ 
+
+    // console.log(gameBoard);
 
 }
 
@@ -71,49 +58,9 @@ function createCardFace(face, card, element){
     element.appendChild(cardElementFace);
 }
 
-function shuffleCards(cards) {
-    let currentIndex = cards.length;
-    let randomIndex = 0;
 
-    while(currentIndex != 0){
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
-    }
-}
 
 createCardsFromTechs(techs);
-
-function createCardsFromTechs(techs){
-
-    let cards = [];
-
-    techs.forEach((tech) => {
-        cards.push(createPairFromTech(tech));
-    })
-    return cards.flatMap(pair => pair);
-
-    console.log(pair, pair);
-}
-
-function createPairFromTech(tech){
-    
-    return [{
-        id:  createIdWithTech(tech),
-        icon: tech,
-        flipped: false
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
-
-function createIdWithTech(tech){
-    return tech + parseInt(Math.random() * 1000);
-}
 
 function flipCard() {
     this.classList.add("flip");
