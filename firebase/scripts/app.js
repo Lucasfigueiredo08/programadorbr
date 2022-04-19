cf229da5d71c3f542df5d15ba36aba6600267cff
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-04-18 22:07:09 -0300

    Projeto firebase

---
+++ firebase/scripts/app.js
@@ -0,0 +1,11 @@
+
+var config = {
+    apiKey: "AIzaSyD46jHltBD9AoStPLVRUF_FwQ3jHOYjSrY",
+    authDomain: "colegio-a6804.firebaseapp.com",
+    projectId: "colegio-a6804",
+    storageBucket: "colegio-a6804.appspot.com",
+    messagingSenderId: "604545120369",
+    appId: "1:604545120369:web:788e89985e7e724b23c3d0",
+    measurementId: "G-5QE0E7EZ9N"
+  };
+firebase.initializeApp(config);
