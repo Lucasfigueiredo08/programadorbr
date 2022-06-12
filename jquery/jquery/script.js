7e4923d32add913b33027db89341f0f262c4e61b
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-06-12 00:08:10 -0300

    encadeando chaining

--- jquery/jquery/script.js
+++ jquery/jquery/script.js
@@ -1,6 +1,24 @@
-let body = $("body");
+// let body = $("body");
 
-console.log(body.find(".item2").fadeOut(1000));
+// console.log(body.find(".item2").fadeOut(1000));
 //lista1.find(".item2").fadeOut(1000);
 
 // lista1.children().fadeOut(1000);
+
+
+//JQUERY - chaining
+
+let jq = $("#lista1").slideUp(2000).slideDown(1000).fadeOut(1000).fadeIn(1000);
+
+// let objeto = {
+//     print10: function(){
+//         console.log(10);
+//         return this;
+//     },
+//     print20: function(){
+//         console.log(20);
+//         return this;
+//     }
+// }
+
+// objeto.print10().print10().print20().print10();
