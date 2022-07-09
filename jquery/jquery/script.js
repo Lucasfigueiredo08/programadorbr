a073237d4b5932d8969c4358e896f4314c2cbd64
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-07-09 17:44:43 -0300

    dimensoes

--- jquery/jquery/script.js
+++ jquery/jquery/script.js
@@ -1,63 +1,7 @@
-// let body = $("body");
+let box = $(".box");
 
-// console.log(body.find(".item2").fadeOut(1000));
-//lista1.find(".item2").fadeOut(1000);
-
-// lista1.children().fadeOut(1000);
-
-
-//JQUERY - chaining
-
-// let jq = $("#lista1").slideUp(2000).slideDown(1000).fadeOut(1000).fadeIn(1000);
-
-// let objeto = {
-//     print10: function(){
-//         console.log(10);
-//         return this;
-//     },
-//     print20: function(){
-//         console.log(20);
-//         return this;
-//     }
-// }
-
-// objeto.print10().print10().print20().print10();
-
-
-// AULA LENDO CONTEUDO DO ELEMENTO
-// console.log($("h1").text())
-//  console.log($("h1").html())
-//  console.log($("#lista1").text())
-//  console.log($("#novo").val())
-  // console.log($("#novo").attr("name"))
-
-  // $("#lista1").find("[key=2]").remove();
-  // $("li").remove(".item2 , .item4")
-// let lista1 = $("#lista1")
-
-// let items = lista1.html()
-
-// lista1.html(item + "<li> novo item </li>")
-
-// lista1.prepend("<li>Novo teste</li>") // after, before, append
-
-
-
-// setTimeout(() => {
-//   $("h1").toggleClass("bg-blue")
-// }, 3000);
-
-// setInterval(() => {
-//   $("h1").toggleClass("bg-blue")
-// }, 300);
-
-$("h1").toggle("bg-blue");
-
-$("h1").css({
-  "background-color":"blue",
-  "font-size": "50px"
-});
-
-
-
+box.append("largura:", box.width(), "<br>",
+          "largura + padding:", box.innerWidth(), "<br>",
+          "largura + padding+ borda:", box.outerWidth(), "<br>",
+          "largura + padding + borda + margin:", box.outerWidth(true));
 
