fbe5b9db1126684b26df644f4ba57192978ec154
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-07-31 15:21:37 -0300

    módulos

---
+++ node-js/calc.js
@@ -0,0 +1,13 @@
+
+
+let calc = {
+    soma: (x, y) => {
+        return x + y;},
+    mult: (x, y) => { 
+        return x * y; },
+    div: (x, y) => { 
+        return x / y; },
+    sub: (x, y) => { 
+        return x - y; }
+}
+module.exports = calc;
--- node-js/script.js
+++ node-js/script.js
@@ -1,31 +1,20 @@
+let calc = require('./calc');
 let args = process.argv.slice(2)
-
-console.log(args)
 
 let a = Number(args[1])
 let b = Number(args[2])
-let c = ""
+let c = "";
 
-if(args[0] === "mult") {	c = a * b } else if 
-(args[0] === "div") { c = a / b } else if
-(args[0] === "sub") { c = a - b } else if
-(args[0] === "sum") { c = a + b } else 
-{ console.log("Invalid operation") }
-
-function soma(x, y) {
-    return x + y;
-}
-
-function mult(x, y) {
-    return x * y;
-}
-
-function div(x, y) {
-    return x / y;
-}
-
-function sub(x, y) {
-    return x - y;
+if(args[0] == 'mult') {	
+    c = calc.mult(a, b) 
+} else if (args[0] == 'div') { 
+    c = calc.div(a, b) 
+} else if(args[0] == 'sub') { 
+    c = calc.sub(a, b)
+ } else if(args[0] == 'sum') {
+     c = calc.soma(a,b) 
+    } else {
+         c = "Operação inválida"
 }
 
 console.log(c)
