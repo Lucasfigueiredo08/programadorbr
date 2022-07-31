895a98c161a9fe06f784fd5f64cbeb8bb1e60e0f
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-07-31 15:51:06 -0300

    modulo aula 2

--- node-js/calc.js
+++ node-js/calc.js
@@ -1,44 +1,58 @@
 
 
-let calc = {
-    soma: (x, y) => {
-        return x + y;},
-    mult: (x, y) => { 
-        return x * y; },
-    div: (x, y) => { 
-        return x / y; },
-    sub: (x, y) => { 
-        return x - y; },
-    mod: (x, y) => {
-        return x % y; },
-    exp: (x, y) => {
-        return x ** y;},
-    raiz: (x) => {
-        return Math.sqrt(x);},
-    log: (x) => {
-        return Math.log(x);},
-    sen: (x) => {
-        return Math.sin(x);},
-    cos: (x) => {
-        return Math.cos(x);},
-    tan: (x) => {
-        return Math.tan(x);},
-    cot: (x) => {
-        return 1 / Math.tan(x);},
-    sec: (x) => {   
-        return 1 / Math.cos(x);},
-    csc: (x) => {
-        return 1 / Math.sin(x);},   
-    asen: (x) => {
-        return Math.asin(x);},
-    acos: (x) => {
-        return Math.acos(x);},  
-    atan: (x) => {
-        return Math.atan(x);},
-    acot: (x) => {
-        return Math.atan(1 / x);}, 
-    asec: (x) => {  
-        return Math.acos(1 / x);},
-}
+// let calc = {
+//     soma: (x, y) => {
+//         return x + y;},
+//     mult: (x, y) => { 
+//         return x * y; },
+//     div: (x, y) => { 
+//         return x / y; },
+//     sub: (x, y) => { 
+//         return x - y; },
+//     mod: (x, y) => {
+//         return x % y; },
+//     exp: (x, y) => {
+//         return x ** y;},
+//     raiz: (x) => {
+//         return Math.sqrt(x);},
+//     log: (x) => {
+//         return Math.log(x);},
+//     sen: (x) => {
+//         return Math.sin(x);},
+//     cos: (x) => {
+//         return Math.cos(x);},
+//     tan: (x) => {
+//         return Math.tan(x);},
+//     cot: (x) => {
+//         return 1 / Math.tan(x);},
+//     sec: (x) => {   
+//         return 1 / Math.cos(x);},
+//     csc: (x) => {
+//         return 1 / Math.sin(x);},   
+//     asen: (x) => {
+//         return Math.asin(x);},
+//     acos: (x) => {
+//         return Math.acos(x);},  
+//     atan: (x) => {
+//         return Math.atan(x);},
+//     acot: (x) => {
+//         return Math.atan(1 / x);}, 
+//     asec: (x) => {  
+//         return Math.acos(1 / x);},
+// }
 
-module.exports = calc;
+// module.exports = calc;
+
+
+//Arrow function
+// module.exports.soma = (x, y) => {
+//     return x + y;
+// }
+
+// module.exports.mult = (x, y) => {   
+//     return x * y;
+// }   
+
+module.exports.soma = function soma(x, y) {
+    return x + y;
+}
--- node-js/script.js
+++ node-js/script.js
@@ -1,43 +1,43 @@
-let calc = require('./calc');
-let args = process.argv.slice(2)
+// let calc = require('./calc');
+// let args = process.argv.slice(2)
 
-let a = Number(args[1])
-let b = Number(args[2])
-let c = "";
+// let a = Number(args[1])
+// let b = Number(args[2])
+// let c = "";
 
-if(args[0] == 'mult') {	
-    c = calc.mult(a, b) 
-} else if (args[0] == 'div') { 
-    c = calc.div(a, b) 
-} else if(args[0] == 'sub') { 
-    c = calc.sub(a, b)
- } else if(args[0] == 'sum') {
-     c = calc.sum(a,b)
-} else if(args[0] == 'sen') {
-        c = calc.sen(a,b) 
-} else if(args[0] == 'cos') {
-        c = calc.cos(a,b) 
-} else if(args[0] == 'tan') {
-        c = calc.tan(a,b) 
-} else if(args[0] == 'cot') {
-        c = calc.cot(a,b) 
-} else if(args[0] == 'sec') {
-        c = calc.sec(a,b) 
-} else if(args[0] == 'asen') {
-        c = calc.asen(a,b) 
-} else if(args[0] == 'asec') {
-        c = calc.asec(a,b) 
-} else if(args[0] == 'acos') {
-        c = calc.acos(a,b) 
-} else if(args[0] == 'atan') {
-        c = calc.atan(a,b) 
-} else if(args[0] == 'acot') {
-        c = calc.acot(a,b) 
-} else if(args[0] == 'csc') {
-        c = calc.csc(a,b) 
-    } else {
-         c = "Operação inválida"
-}
+// if(args[0] == 'mult') {	
+//     c = calc.mult(a, b) 
+// } else if (args[0] == 'div') { 
+//     c = calc.div(a, b) 
+// } else if(args[0] == 'sub') { 
+//     c = calc.sub(a, b)
+//  } else if(args[0] == 'sum') {
+//      c = calc.sum(a,b)
+// } else if(args[0] == 'sen') {
+//         c = calc.sen(a,b) 
+// } else if(args[0] == 'cos') {
+//         c = calc.cos(a,b) 
+// } else if(args[0] == 'tan') {
+//         c = calc.tan(a,b) 
+// } else if(args[0] == 'cot') {
+//         c = calc.cot(a,b) 
+// } else if(args[0] == 'sec') {
+//         c = calc.sec(a,b) 
+// } else if(args[0] == 'asen') {
+//         c = calc.asen(a,b) 
+// } else if(args[0] == 'asec') {
+//         c = calc.asec(a,b) 
+// } else if(args[0] == 'acos') {
+//         c = calc.acos(a,b) 
+// } else if(args[0] == 'atan') {
+//         c = calc.atan(a,b) 
+// } else if(args[0] == 'acot') {
+//         c = calc.acot(a,b) 
+// } else if(args[0] == 'csc') {
+//         c = calc.csc(a,b) 
+//     } else {
+//          c = "Operação inválida"
+// }
 
-console.log(c)
+// console.log(c)
 
