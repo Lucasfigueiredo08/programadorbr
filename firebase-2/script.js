f9b942b5e572b14b5ad398df39b6f9affa4c1a37
Lucas Matheus Liberato Figueiredo <lfiqueiredo@rodalog.corp>
2022-08-01 16:25:19 -0300

    servidor de paginas com node

---
+++ firebase-2/script.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBDAH8tisq6X1wtgcymDEKk0cnNOESTcRM",
+    authDomain: "teste-1f4e0.firebaseapp.com",
+    projectId: "teste-1f4e0",
+    storageBucket: "teste-1f4e0.appspot.com",
+    messagingSenderId: "945743194282",
+    appId: "1:945743194282:web:3adc563330d39a5d8fc8d3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
+
--- node-js/http/index.js
+++ node-js/http/index.js
@@ -1,11 +1,32 @@
  const http = require('http');
+ const url = require('url');
+ const fs = require('fs');
 
  http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify({texto: "<h1>Hello World</h1>"}));
- }).listen(3000, (err) => {
+   //  response.writeHead(200, {'Content-Type': 'application/json'});
+   //  response.end(JSON.stringify({texto: "<h1>Hello World</h1>"}));
+
+   let path = url.parse(request.url).pathname;
+   
+
+   if(path == "" || path == "/") { 
+       path = "/index.html";
+   }
+   let fileName = "." + path;
+
+   fs.readFile(fileName, (err, data) => {
+     if (err) {
+       response.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'});
+       response.end("<h1>404 Not Found</h1>");
+     } else {
+       response.writeHead(200, {'Content-Type': 'text/html'});
+       response.end(data);
+     }
+   });
+
+   }).listen(3000, (err) => {
     if (err) {
       return console.log('something bad happened', err)
     }
     console.log('server is listening on 3000')
- });
+ })
