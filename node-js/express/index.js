8c5d51baef2682f8edbc281273e62be353b9cdd2
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-08-06 17:50:41 -0300

    arquivos estaticos com express

--- node-js/express/index.js
+++ node-js/express/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const PORT = 5000;
+
+app.use("/meusite", express.static(path.join(__dirname, 'client')));
 
 app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
