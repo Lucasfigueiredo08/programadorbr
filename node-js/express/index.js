bbd56027cc958c891f70cd90eb343cbe89454e79
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-08-06 18:04:10 -0300

    express - get

--- node-js/express/index.js
+++ node-js/express/index.js
@@ -7,6 +7,11 @@ const PORT = 5000;
 
 app.use("/meusite", express.static(path.join(__dirname, 'client')));
 
+app.get('/', (req, res) => {
+        res.type("html");
+        res.send("<h1>Hello World from GET</h1>");
+})
+
 app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
 })
