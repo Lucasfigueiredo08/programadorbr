7158777f57559e80bebbedb569e73b72bdb336ca
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-08-07 17:32:56 -0300

    criando rotas de posts

--- node-js/meumural/index.js
+++ node-js/meumural/index.js
@@ -16,9 +16,17 @@ app.get("/all", (req, res) => {
     res.json(posts);
 })
 
-app.post("/new", (req, res) => {
-    
+app.post("/new", bodyParser.json(), (req, res) => {
+    let id = generateId();
+    let title = req.body.title;
+    let description = req.body.description;
+    posts.push({id, title, description})
+    res.send("Post adicionado com sucesso");
 })
+
+function generateId() {
+    return Math.random().toString(36).substring(2,9);
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
