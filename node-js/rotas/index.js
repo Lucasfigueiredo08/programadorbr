4edf681f814a4a54c63b7a4bca82780dbdf82c4e
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-08-07 16:25:09 -0300

    rotas

--- node-js/rotas/index.js
+++ node-js/rotas/index.js
@@ -2,38 +2,35 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+let alunos = [{
+    id: 0, name: "Jose"},
+    {id: 1, name: "Maria"},
+    {id: 2, name: "Joao"}];
+
+app.use(bodyParser.urlencoded( { extended: true } ));
 
 app.get('/', function (req, res) {
     res.send("Hello World!");
 })
 
-app.get("/alunos", (res, req) => {
-
-    let alunos = [{
-        id: 0, name: "Jose"},
-        {id: 1, name: "Maria"},
-        {id: 2, name: "Joao"}];
-
+app.get("/alunos", (req, res) => {
         res.json(JSON.stringify(alunos));
 })
 
-app.get("/aluno/:id", (res,req) => {
+// app.get("/aluno", (res,req) => {
+//     console.log(req.body);
+//     console.log(req.query);
+//     let aluno = alunos[req.body.id];
+//     res.json(aluno);
+// })
 
-    console.log(req.body);
-    consolge.log(req.query);
-    // /alunos/2
-    // let aluno = [req.params.id]
-
-    // /aluno?id=1
-    let aluno = aluno[req.query.id];
-
-    // /aluno
-    // let aluno = aluno[req.body.id];
+app.get("/aluno/:id", (req, res) => {
+    console.log(req.params.id);
+    let aluno = alunos[req.params.id];
     res.json(aluno);
 })
 
-app.listen(3000, () => {
+app.listen(5000, () => {
 
-    console.log("Servidor rodando na porta 3000");
+    console.log("Servidor rodando na porta 5000");
 })
