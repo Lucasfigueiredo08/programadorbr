e25474484580a4b44132df09ca52ed37405284d4
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-08-16 23:42:30 -0300

    for no ejs

--- template-ejs/index.js
+++ template-ejs/index.js
@@ -3,11 +3,22 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+let users = [{
+    name: 'John Doe',
+    phone: '+3809912345678',
+}, {
+    name: 'Jane Doe',
+    phone: '+3809987654321',
+}, {
+    name: 'Jack Doe',
+    phone: '+3809976543210',
+}];
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render('user')
+    res.render('user', {users}/*{name: 'John Doe', phone: '+380991234567'}*/);
 });
 
 app.listen(3000, () => {
